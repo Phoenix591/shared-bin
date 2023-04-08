fc9057399657dbf61ff2baa4b580d62e2c003d47
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2023-04-08 00:09:58 -0500

    cloudflare.py: use the config file more reads subdomains and ipv6 configuration from config

--- cloudflare.py
+++ cloudflare.py
@@ -10,7 +10,15 @@ import os
 config = configparser.ConfigParser()
 configfile = os.path.expanduser('~/.cloudflare/my-script.ini')
 config.read(configfile)
-myconfig = config['DEFAULT']
+
+# config example
+#[domain.com]
+#zone_name = domain.com
+#zone_id = (cloudflare zone id)
+#subdomains = foo bar
+#ipv6 = false
+
+
 
 def getip(type):
     ip=subprocess.run(["dig", type, "+short", "TXT", "o-o.myaddr.l.google.com", "@ns1.google.com"], capture_output=True, universal_newlines=True)
@@ -21,31 +29,38 @@ def dropq(var):
     var=var.rstrip('\n')
     return var
 
-
 def main():
     ip4=getip("-4")
     ip6=getip("-6")
-    doip6=False
     ip4=dropq(ip4)
     for section in config.sections():
         myconfig = config[section]
         zone_name = myconfig['zone_name']
         zone_id = myconfig['zone_id']
+        doip6 = myconfig.getboolean('ipv6')
         cf = CloudFlare.CloudFlare(profile=myconfig['zone_name'])
+
+        dns_records = [
+            {'name':myconfig['zone_name'], 'type':'A', 'content':ip4},
+            ]
 
         if doip6==True:
             ip6=dropq(ip6)
-            dns_records = [
-                {'name':'dns.'+myconfig['zone_name'], 'type':'AAAA', 'content':ip6},
-                {'name':'dns.'+myconfig['zone_name'], 'type':'A', 'content':ip4},
-                {'name':myconfig['zone_name'], 'type':'A', 'content':ip4},
-                {'name':myconfig['zone_name'], 'type':'AAAA', 'content':ip6},
+            v6_records = [
+                {'name':myconfig['zone_name'], 'type':'AAAA', 'content':ip6}
             ]
-        else:
-            dns_records = [
-                {'name':'dns.'+myconfig['zone_name'], 'type':'A', 'content':ip4},
-                {'name':myconfig['zone_name'], 'type':'A', 'content':ip4},
+            dns_records.extend(v6_records)
+        subdomains=myconfig['subdomains'].split()
+        for subdomain in subdomains:
+            subd_record = [
+                {'name':subdomain+'.'+myconfig['zone_name'], 'type':'A', 'content':ip4}
             ]
+            if doip6==True:
+                subd_record = subd_record + [
+                    {'name':subdomain+'.'+myconfig['zone_name'], 'type':'AAAA', 'content':ip6}
+                    ]
+            dns_records.extend(subd_record)
+
         def getrecord(name, ipv):
             record=cf.zones.dns_records.get(zone_id,params={'name':name, 'type':ipv})
             return record
