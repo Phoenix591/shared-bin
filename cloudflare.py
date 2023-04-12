7499c7e019c470b076fd409c6650083e02a7faa0
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2023-04-12 06:29:03 -0500

    cloudflare.py: ran through black code formatter

--- cloudflare.py
+++ cloudflare.py
@@ -2,91 +2,107 @@
 
 import sys
 import CloudFlare
-import  subprocess
+import subprocess
 import configparser
 import os
 
 
 config = configparser.ConfigParser()
-configfile = os.path.expanduser('~/.cloudflare/my-script.ini')
+configfile = os.path.expanduser("~/.cloudflare/my-script.ini")
 config.read(configfile)
 
 # config example
-#[domain.com]
-#zone_name = domain.com
-#zone_id = (cloudflare zone id)
-#subdomains = foo bar
-#ipv6 = false
-
+# [domain.com]
+# zone_name = domain.com
+# zone_id = (cloudflare zone id)
+# subdomains = foo bar
+# ipv6 = false
 
 
 def getip(type):
-    ip=subprocess.run(["dig", type, "+short", "TXT", "o-o.myaddr.l.google.com", "@ns1.google.com"], capture_output=True, universal_newlines=True)
+    ip = subprocess.run(
+        ["dig", type, "+short", "TXT", "o-o.myaddr.l.google.com", "@ns1.google.com"],
+        capture_output=True,
+        universal_newlines=True,
+    )
     return ip.stdout
 
+
 def dropq(var):
-    var=var.replace('"', '')
-    var=var.rstrip('\n')
+    var = var.replace('"', "")
+    var = var.rstrip("\n")
     return var
 
+
 def main():
-    ip4=getip("-4")
-    ip6=getip("-6")
-    ip4=dropq(ip4)
+    ip4 = getip("-4")
+    ip6 = getip("-6")
+    ip4 = dropq(ip4)
     for section in config.sections():
         myconfig = config[section]
-        zone_name = myconfig['zone_name']
-        cf = CloudFlare.CloudFlare(profile=myconfig['zone_name'])
+        zone_name = myconfig["zone_name"]
+        cf = CloudFlare.CloudFlare(profile=myconfig["zone_name"])
 
         try:
-            zone_id = myconfig['zone_id']
+            zone_id = myconfig["zone_id"]
         except KeyError:
-            print('zone_id not specified, querying the api')
-            params= { 'name': myconfig['zone_name']}
-            zone=cf.zones.get(params=params)
-            zone_id=zone[0]['id']
-        doip6 = myconfig.getboolean('ipv6')
+            print("zone_id not specified, querying the api")
+            params = {"name": myconfig["zone_name"]}
+            zone = cf.zones.get(params=params)
+            zone_id = zone[0]["id"]
+        doip6 = myconfig.getboolean("ipv6")
 
         dns_records = [
-            {'name':myconfig['zone_name'], 'type':'A', 'content':ip4},
-            ]
+            {"name": myconfig["zone_name"], "type": "A", "content": ip4},
+        ]
 
-        if doip6==True:
-            ip6=dropq(ip6)
+        if doip6 == True:
+            ip6 = dropq(ip6)
             v6_records = [
-                {'name':myconfig['zone_name'], 'type':'AAAA', 'content':ip6}
+                {"name": myconfig["zone_name"], "type": "AAAA", "content": ip6}
             ]
             dns_records.extend(v6_records)
-        subdomains=myconfig['subdomains'].split()
+        subdomains = myconfig["subdomains"].split()
         for subdomain in subdomains:
             subd_record = [
-                {'name':subdomain+'.'+myconfig['zone_name'], 'type':'A', 'content':ip4}
+                {
+                    "name": subdomain + "." + myconfig["zone_name"],
+                    "type": "A",
+                    "content": ip4,
+                }
             ]
-            if doip6==True:
+            if doip6 == True:
                 subd_record = subd_record + [
-                    {'name':subdomain+'.'+myconfig['zone_name'], 'type':'AAAA', 'content':ip6}
-                    ]
+                    {
+                        "name": subdomain + "." + myconfig["zone_name"],
+                        "type": "AAAA",
+                        "content": ip6,
+                    }
+                ]
             dns_records.extend(subd_record)
 
         def getrecord(name, ipv):
-            record=cf.zones.dns_records.get(zone_id,params={'name':name, 'type':ipv})
+            record = cf.zones.dns_records.get(
+                zone_id, params={"name": name, "type": ipv}
+            )
             return record
 
         def getrecordid(name, ipv):
-            record=getrecord(name, ipv)
-            record=record[0]
-            return(record['id'])
+            record = getrecord(name, ipv)
+            record = record[0]
+            return record["id"]
 
-        print('zone id: '+zone_id)
+        print("zone id: " + zone_id)
 
         for dns_record in dns_records:
-            #print(ip4)
-            print('record: ', end = '')
+            # print(ip4)
+            print("record: ", end="")
             print(dns_record)
-            record_id=getrecordid(dns_record['name'], dns_record['type'])
-            print('record id: '+record_id)
-            r = cf.zones.dns_records.put(zone_id, record_id,  data=dns_record)
+            record_id = getrecordid(dns_record["name"], dns_record["type"])
+            print("record id: " + record_id)
+            r = cf.zones.dns_records.put(zone_id, record_id, data=dns_record)
     exit(0)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
