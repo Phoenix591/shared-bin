1fe2212ed1619da885630de883d6c3dd3357b22b
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2024-02-13 00:41:57 -0600

    postsync.sh: catch stderr like conflicts

--- postsync.sh
+++ postsync.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-emerge -pvuDU world >/tmp/post-sync
+emerge -pvuDU world 2>&1 | tee /tmp/post-sync
+cat /usr/portage/metadata/timestamp.chk >>/tmp/post-sync
 if ! grep -q "0 packages" /tmp/post-sync; then
-	(cat /srv/general/portage.txt; cat /tmp/post-sync) |sendmail $1
+	(cat /srv/general/portage.txt; cat /tmp/post-sync) |sendmail $1 && \
+	echo "Mail sent"
 fi
