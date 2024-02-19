da902ce86fa0aba2946e3cff5330e91da2527f48
Phoenix591 <11486+Phoenix591@users.noreply.github.com>
2024-02-19 16:50:46 -0600

    postsync.sh: fix bug if x0 upgrades

--- postsync.sh
+++ postsync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 emerge -pvuDU world 2>&1 | tee /tmp/post-sync
 cat /usr/portage/metadata/timestamp.chk >>/tmp/post-sync
-if ! grep -q "0 packages" /tmp/post-sync; then
+if ! grep -wq "0 packages" /tmp/post-sync; then
 	(cat /srv/general/portage.txt; cat /tmp/post-sync) |sendmail $1 && \
 	echo "Mail sent"
 fi
